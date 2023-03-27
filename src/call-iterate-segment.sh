caa3b32c75c0e8fb48a4aec3d21156956a64a687
radio-ais <help@continent1.net>
2023-03-27 15:55:50 +0900

    on iterate-20230327

---
+++ src/call-iterate-segment.sh
@@ -0,0 +1,4 @@
+python3 iterate-segment-multiple-patients.py --srcrootdir=/home/ubuntu/data/dcm --targetrootdir=/home/ubuntu/works/generate-nrrd-20230306/results
+
+# syntax iterate-segment-multiple-patients.py --srcrootdir=[] --targetrootdir=[]
+
